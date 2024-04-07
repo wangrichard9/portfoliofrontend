5336933d423220d56cf103dd1acca49288ca78ed
wangrichard9 <162049929+wangrichard9@users.noreply.github.com>
2024-04-07 09:48:22 -0700

    Update works.js

--- src/pages/Works/works.js
+++ src/pages/Works/works.js
@@ -11,9 +11,9 @@ const Works = () => {
             <h2 className="worksTitle"> My Projects</h2>
             <span className = "worksDesc">Here are a list of a couple of projects I have made with various tools </span>
             <div className="worksImgs">
-                <a href="https://bootstrappaintings.netlify.app/"><img src={Bootstrap} alt="" className="worksImg" /></a>
+                <a href="https://bootstrappaintings.netlify.app/" target="_blank"><img src={Bootstrap} alt="" className="worksImg" /></a>
                 <img src={Todo} alt="" className="worksImg" />
-                <a href="https://memorygameprojectt.netlify.app/"><img src={Memory} alt="" className="worksImg" /></a>
+                <a href="https://memorygameprojectt.netlify.app/" target="_blank"><img src={Memory} alt="" className="worksImg" /></a>
                 <img src={Weather} alt="" className="worksImg" />
             </div>
         </section>
